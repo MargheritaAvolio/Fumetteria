939dfcd81676a5eb26f9ecc657f5e1e396b30f9d
Margherita <margheritavolio.ma@gmail.com>
2022-05-02 09:26:35 +0200

    Aggiunta logica al Form di Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {  //loginpage COMPONENTE ha una proprietà (ciao:string)
 
   titolo:string = "LoginPage Fumetteria DevSkill"
+  
+  username:string="";
+  password: string="";
+
 
   constructor() { }
 
-  
+  clickEvento(): void{
+    
+    if (!this.username && !this.password){
+      alert("Il nome utente o la password che hai inserito non sono corretti!  \nPer favore inserisci di nuovo i tuoi dati")
+    }else if (!this.username && this.password){
+      alert("Per accedere inserisci nuovamente l'username")
+    }else if (this.username && !this.password){
+      alert("Ciao " + this.username + "\nPer accedere inserisci nuovamente la password")
+    }else
+      alert("Ciao " + this.username + "!");
+    }
+
+
 
   ngOnInit() {}
 
